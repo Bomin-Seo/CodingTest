eed733429cb8ba6345de3d8ac88c043c093dc8ed
Bomin-Seo <94039896+Bomin-Seo@users.noreply.github.com>
2022-09-28 10:25:09 +0900

    Add files via upload

---
+++ Programmers/Level1/Python/programmers_lv1_최대공약수와최소공배수.py
@@ -0,0 +1,12 @@
+def solution(n, m):
+    answer = []
+    x, y = n, m
+    while y:
+        x, y = y, x%y
+
+    answer.append(x)
+
+    answer.append(n*m // x )
+    return answer
+
+print(solution(2, 5))
